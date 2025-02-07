0841153fe6fb1fc88720a1372cd9ec050c9761cc
AatirNadim <aatir.nadim@gmail.com>
2025-02-08 00:52:34 +0530

    working on the welcome message (Welcome to Aikame #3)

--- aikame.py
+++ aikame.py
@@ -4,24 +4,39 @@ import os
 from utils.crud_files import load_files, show_files, clear_context, remove_file
 from utils.rag import query
 
+
 @click.group()
 @click.pass_context
 def cli(ctx: click.Context) -> None :
-	ctx.obj = {}
+  """
+  ####   #   #    #    ####   #   #   ####
+     #       #   #        #   ## ##   #   #
+  ####   #   #  #      ####   # # #   #####
+ #   #   #   ###      #   #   #   #   #
+ #   #   #   #  #     #   #   #   #   #   #
+  ####   #   #   #     ####   #   #   ####
+
+  Aikame is a robust RAG application brought to you as a cli tool.
+  """
+
+  click.secho("Welcome to Aikame!", fg='green')
+
 
 @cli.command()
 @click.pass_context
 def set_value(ctx):
-    """Set a value in the context."""
-    os.environ['value'] = "Hello, World!"  # Store a value in the context
-    click.echo("Value set!")
+  """Set a value in the context."""
+  os.environ['value'] = "Hello, World!"  # Store a value in the context
+  click.echo("Value set!")
+
 
 @cli.command()
 @click.pass_context
 def get_value(ctx):
-    """Get the value from the context."""
-    value = os.getenv('value', 'No value set.')
-    click.echo(f'Value: {value}')
+  """Get the value from the context."""
+  value = os.getenv('value', 'No value set.')
+  click.echo(f'Value: {value}')
+
 
 cli.add_command(hello)
 cli.add_command(load_files)
@@ -34,4 +49,4 @@ cli.add_command(query)
 
 
 if __name__ == "__main__":
-	cli()
+  cli()
